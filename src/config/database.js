9a806ec487d3d013eced312ddf2b3a86e5aab4fb
Vitor Hugo <vitor95340@gmail.com>
2026-03-09 15:24:09 -0300

    [update: database] - adicionando ssl para subir o banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false, // Necessário para conectar ao Aiven e bancos em nuvem
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
